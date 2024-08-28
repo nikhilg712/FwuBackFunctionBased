69fda4c24fa51c67546ef12fc3549f16d613fb6b
bhartimishra-fwu <BhartiMishra@flewwithus.com>
2024-08-28 15:17:26 +0530

    Fix: flight search changes

--- src/services/home.service.ts
+++ src/services/home.service.ts
@@ -269,42 +269,43 @@ const searchFlights = async (
       const flightNumber = flight?.Segments[0][0].Airline.FlightNumber;
       return lowestFareFlights[flightNumber] === flight;
     });
-    const data: Root[] = [lowestFareFlightsArray];
-    const mappedFlightInfo: SelectedFareQuote[] = data.map((flight) => ({
-      FirstNameFormat: flight.FirstNameFormat,
-      IsBookableIfSeatNotAvailable: flight.IsBookableIfSeatNotAvailable,
-      IsHoldAllowedWithSSR: flight.IsHoldAllowedWithSSR,
-      IsUpsellAllowed: flight.IsUpsellAllowed,
-      LastNameFormat: flight.LastNameFormat,
-      ResultIndex: flight.ResultIndex,
-      Source: flight.Source,
-      IsLCC: flight.IsLCC,
-      IsRefundable: flight.IsRefundable,
-      IsPanRequiredAtBook: flight.IsPanRequiredAtBook,
-      IsPanRequiredAtTicket: flight.IsPanRequiredAtTicket,
-      IsPassportRequiredAtBook: flight.IsPassportRequiredAtBook,
-      IsPassportRequiredAtTicket: flight.IsPassportRequiredAtTicket,
-      GSTAllowed: flight.GSTAllowed,
-      IsCouponAppilcable: flight.IsCouponAppilcable,
-      IsGSTMandatory: flight.IsGSTMandatory,
-      AirlineRemark: flight.AirlineRemark,
-      IsPassportFullDetailRequiredAtBook:
-        flight.IsPassportFullDetailRequiredAtBook,
-      ResultFareType: flight.ResultFareType,
-      Fare: flight.Fare,
-      FareBreakdown: flight.FareBreakdown,
-      Segments: flight.Segments,
-      LastTicketDate: flight.LastTicketDate,
-      TicketAdvisory: flight.TicketAdvisory,
-      FareRules: flight.FareRules,
-      PenaltyCharges: flight.PenaltyCharges,
-      AirlineCode: flight.AirlineCode,
-      MiniFareRules: flight.MiniFareRules,
-      ValidatingAirline: flight.ValidatingAirline,
-      FareClassification: flight.FareClassification,
-    }));
+    let data: Root[] = [lowestFareFlightsArray];
+    // data = data[0];
+    // const mappedFlightInfo: SelectedFareQuote[] = data.map((flight) => ({
+    //   FirstNameFormat: flight.FirstNameFormat,
+    //   IsBookableIfSeatNotAvailable: flight.IsBookableIfSeatNotAvailable,
+    //   IsHoldAllowedWithSSR: flight.IsHoldAllowedWithSSR,
+    //   IsUpsellAllowed: flight.IsUpsellAllowed,
+    //   LastNameFormat: flight.LastNameFormat,
+    //   ResultIndex: flight.ResultIndex,
+    //   Source: flight.Source,
+    //   IsLCC: flight.IsLCC,
+    //   IsRefundable: flight.IsRefundable,
+    //   IsPanRequiredAtBook: flight.IsPanRequiredAtBook,
+    //   IsPanRequiredAtTicket: flight.IsPanRequiredAtTicket,
+    //   IsPassportRequiredAtBook: flight.IsPassportRequiredAtBook,
+    //   IsPassportRequiredAtTicket: flight.IsPassportRequiredAtTicket,
+    //   GSTAllowed: flight.GSTAllowed,
+    //   IsCouponAppilcable: flight.IsCouponAppilcable,
+    //   IsGSTMandatory: flight.IsGSTMandatory,
+    //   AirlineRemark: flight.AirlineRemark,
+    //   IsPassportFullDetailRequiredAtBook:
+    //     flight.IsPassportFullDetailRequiredAtBook,
+    //   ResultFareType: flight.ResultFareType,
+    //   Fare: flight.Fare,
+    //   FareBreakdown: flight.FareBreakdown,
+    //   Segments: flight.Segments,
+    //   LastTicketDate: flight.LastTicketDate,
+    //   TicketAdvisory: flight.TicketAdvisory,
+    //   FareRules: flight.FareRules,
+    //   PenaltyCharges: flight.PenaltyCharges,
+    //   AirlineCode: flight.AirlineCode,
+    //   MiniFareRules: flight.MiniFareRules,
+    //   ValidatingAirline: flight.ValidatingAirline,
+    //   FareClassification: flight.FareClassification,
+    // }));
 
-    return mappedFlightInfo;
+    return data;
   } catch (err: any) {
     console.error("Service Error:", err);
     next(new AppError(err.message, 500));
